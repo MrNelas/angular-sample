bfb7b56d6f0d0ff3613ac6a24504eca894878ca9
Ivan Tkachenko <nel18@yandex.ru>
2023-08-03 00:49:17 +0300

    форматирование

--- src/app/modules/angular/components/change-detection/change-detection.component.ts
+++ src/app/modules/angular/components/change-detection/change-detection.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-change-detection',
   templateUrl: './change-detection.component.html',
-  styleUrls: ['./change-detection.component.scss']
+  styleUrls: ['./change-detection.component.scss'],
 })
-export class ChangeDetectionComponent {
-
-}
+export class ChangeDetectionComponent {}
